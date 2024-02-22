663ad7d4bfa43c1b6e46b902f5be532c61498d5a
Alexander Frick <Alex313031@gmail.com>
2024-02-22 03:25:34 -0600

    fix debian

--- post-ubuntu.sh
+++ post-ubuntu.sh
@@ -65,7 +65,7 @@ installCommon () {
 	cd &&
 	sudo apt update &&
 	sudo apt full-upgrade &&
-	sudo apt install autoconf autoconf-archive automake autopoint autotools-dev beep binfmt-support build-essential cmake cpufrequtils cpuid cpustat cputool curl dconf-editor debhelper debian-keyring desktop-file-utils devhelp dh-autoreconf dh-make dh-strip-nondeterminism dkms docbook doc-base docutils-common elfutils exfat-fuse exo-utils fancontrol fdutils ffmpeg ffmpeg-doc ffmpegthumbnailer galternatives gimp gimp-data-extras gimp-help-en git git-gui gitk gmrun gnome-shell-extensions gnu-standards gpart gparted hardinfo hwloc i2c-tools icoutils intel-cmt-cat intel-gpu-tools iotop isag libcanberra-gtk-module libdrm-dev libdvdcss2 libfile-stripnondeterminism-perl libgtk-3-dev libhwloc-common libhwloc-dev libhwloc-plugins libi2c-dev libltdl-dev libmail-sendmail-perl libncurses5-dev libncursesw5-dev libportaudio2 libqt5opengl5 libqt5printsupport5 librrds-perl libsensor-msgs-dev libsensors-applet-plugin-dev libsensors4-dev libsigsegv2 libsys-hostname-long-perl libsystemd-dev libtool libudev-dev libvdpau-va-gl1 libvulkan-dev libvulkan1 libxml2-dev libxml2-utils lm-sensors lksctp-tools m4 meld menu menu-l10n mesa-utils mpv mtools numlockx openvpn overlay-scrollbar overlay-scrollbar-gtk2 p7zip-full po-debconf python3-docutils python3-oauth2client python3-pkg-resources python3-setuptools python3-tk python3-virtualenv qt5-image-formats-plugins qtwayland5 read-edid resolvconf rrdtool sl sndiod squashfs-tools squashfuse stress-ng stunnel4 sysstat tcl-tclreadline terminator texinfo thunar thunar-archive-plugin thunar-media-tags-plugin tk tree tumbler-plugins-extra ubuntu-restricted-addons ubuntu-restricted-extras ubuntu-sounds unrar v86d vainfo vdpau-driver-all vdpauinfo virtualenv vlc wget x11-utils xfce4-settings xz-utils zstd libsecret* &&
+	sudo apt install autoconf autoconf-archive automake autopoint autotools-dev beep binfmt-support build-essential cmake cpufrequtils cpuid cpustat cputool curl dconf-editor debhelper debian-keyring desktop-file-utils devhelp dh-autoreconf dh-make dh-strip-nondeterminism dkms docbook doc-base docutils-common elfutils exfat-fuse exo-utils fancontrol fdutils ffmpeg ffmpeg-doc ffmpegthumbnailer galternatives gimp gimp-data-extras gimp-help-en git git-gui gitk gmrun gnome-shell-extensions gnu-standards gpart gparted hardinfo hwloc i2c-tools icoutils intel-cmt-cat intel-gpu-tools iotop isag libcanberra-gtk-module libdrm-dev libdvdcss2 libfile-stripnondeterminism-perl libgtk-3-dev libhwloc-common libhwloc-dev libhwloc-plugins libi2c-dev libltdl-dev libmail-sendmail-perl libncurses5-dev libncursesw5-dev libportaudio2 libqt5opengl5 libqt5printsupport5 librrds-perl libsensor-msgs-dev libsensors-applet-plugin-dev libsensors4-dev libsigsegv2 libsys-hostname-long-perl libsystemd-dev libtool libudev-dev libvdpau-va-gl1 libvulkan-dev libvulkan1 libxml2-dev libxml2-utils lm-sensors lksctp-tools m4 meld menu menu-l10n mesa-utils mpv mtools numlockx openvpn p7zip-full po-debconf python3-docutils python3-oauth2client python3-pkg-resources python3-setuptools python3-tk python3-virtualenv qt5-image-formats-plugins qtwayland5 read-edid resolvconf rrdtool sl sndiod squashfs-tools squashfuse stress-ng stunnel4 sysstat tcl-tclreadline terminator texinfo thunar thunar-archive-plugin thunar-media-tags-plugin tk tree tumbler-plugins-extra ubuntu-restricted-addons ubuntu-restricted-extras ubuntu-sounds unrar v86d vainfo vdpau-driver-all vdpauinfo virtualenv vlc wget x11-utils xfce4-settings xz-utils zstd libsecret* &&
 
 	sudo dpkg-reconfigure libdvd-pkg &&
 	
@@ -86,7 +86,7 @@ installCommonDebian () {
 	cd &&
 	sudo apt update &&
 	sudo apt full-upgrade &&
-	sudo apt install autoconf autoconf-archive automake autopoint autotools-dev beep binfmt-support build-essential cmake cpufrequtils cpuid cpustat cputool curl dconf-editor debhelper debian-keyring desktop-file-utils devhelp dh-autoreconf dh-make dh-strip-nondeterminism dkms docbook doc-base docutils-common elfutils exfat-fuse exo-utils fancontrol fdutils ffmpeg ffmpeg-doc ffmpegthumbnailer galternatives gimp gimp-data-extras gimp-help-en git git-gui gitk gmrun gnome-shell-extensions gnu-standards gpart gparted hardinfo hwloc i2c-tools icoutils intel-cmt-cat intel-gpu-tools iotop isag libcanberra-gtk-module libdrm-dev libdvdcss2 libfile-stripnondeterminism-perl libgtk-3-dev libhwloc-common libhwloc-dev libhwloc-plugins libi2c-dev libltdl-dev libmail-sendmail-perl libncurses5-dev libncursesw5-dev libportaudio2 libqt5opengl5 libqt5printsupport5 librrds-perl libsensor-msgs-dev libsensors-applet-plugin-dev libsensors4-dev libsigsegv2 libsys-hostname-long-perl libsystemd-dev libtool libudev-dev libvdpau-va-gl1 libvulkan-dev libvulkan1 libxml2-dev libxml2-utils lm-sensors lksctp-tools m4 meld menu menu-l10n mesa-utils mpv mtools numlockx openvpn overlay-scrollbar overlay-scrollbar-gtk2 p7zip-full po-debconf python3-docutils python3-oauth2client python3-pkg-resources python3-setuptools python3-tk python3-virtualenv qt5-image-formats-plugins qtwayland5 read-edid resolvconf rrdtool sl sndiod squashfs-tools squashfuse stress-ng stunnel4 sysstat tcl-tclreadline terminator texinfo thunar thunar-archive-plugin thunar-media-tags-plugin tk tree tumbler-plugins-extra libavcodec-extra libavcodec-extra59 ttf-mscorefonts-installer gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi unrar v86d vainfo vdpau-driver-all vdpauinfo virtualenv vlc wget x11-utils xfce4-settings xz-utils zstd libsecret* &&
+	sudo apt install autoconf autoconf-archive automake autopoint autotools-dev beep binfmt-support build-essential cmake cpufrequtils cpuid cpustat cputool curl dconf-editor debhelper debian-keyring desktop-file-utils devhelp dh-autoreconf dh-make dh-strip-nondeterminism dkms docbook doc-base docutils-common elfutils exfat-fuse exo-utils fancontrol fdutils ffmpeg ffmpeg-doc ffmpegthumbnailer galternatives gimp gimp-data-extras gimp-help-en git git-gui gitk gmrun gnome-shell-extensions gnu-standards gpart gparted hardinfo hwloc i2c-tools icoutils intel-cmt-cat intel-gpu-tools iotop isag libcanberra-gtk-module libdrm-dev libdvdcss2 libfile-stripnondeterminism-perl libgtk-3-dev libhwloc-common libhwloc-dev libhwloc-plugins libi2c-dev libltdl-dev libmail-sendmail-perl libncurses5-dev libncursesw5-dev libportaudio2 libqt5opengl5 libqt5printsupport5 librrds-perl libsensor-msgs-dev libsensors-applet-plugin-dev libsensors4-dev libsigsegv2 libsys-hostname-long-perl libtool libvdpau-va-gl1 libvulkan-dev libvulkan1 libxml2-dev libxml2-utils lm-sensors lksctp-tools m4 meld menu menu-l10n mesa-utils mpv mtools numlockx openvpn overlay-scrollbar overlay-scrollbar-gtk2 p7zip-full po-debconf python3-docutils python3-oauth2client python3-pkg-resources python3-setuptools python3-tk python3-virtualenv qt5-image-formats-plugins qtwayland5 read-edid resolvconf rrdtool sl sndiod squashfs-tools squashfuse stress-ng stunnel4 sysstat tcl-tclreadline terminator texinfo thunar thunar-archive-plugin thunar-media-tags-plugin tk tree tumbler-plugins-extra libavcodec-extra libavcodec-extra59 ttf-mscorefonts-installer gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi unrar vainfo vdpau-driver-all vdpauinfo virtualenv vlc wget x11-utils xfce4-settings xz-utils zstd libsecret* &&
 
 	sudo dpkg-reconfigure libdvd-pkg &&
 	
@@ -195,7 +195,7 @@ installDebian () {
 	tput sgr0 &&
 	
 	cd &&
-	sudo apt install intel-media-va-driver-non-free libappimage1.0abi1 libappimage-dev exfatprogs mesa-va-drivers python3-smbus i965-va-driver-shaders gnome-tweaks qt5-qmltooling-plugins neofetch vulkan-tools libarchive-cpio-perl sse3-support libexo-2-dev libvte-2.91-0 libu2f-udev python2 python2-dev python2.7-dev python2-minimal python2-pip-whl python2-setuptools-whl gstreamer1.0-plugins-bad &&
+	sudo apt install intel-media-va-driver-non-free libappimage1.0abi1 libappimage-dev exfatprogs mesa-va-drivers python3-smbus i965-va-driver-shaders gnome-tweaks qt5-qmltooling-plugins neofetch vulkan-tools libarchive-cpio-perl sse3-support libexo-2-dev libvte-2.91-0 libu2f-udev python python3-dev python3-wheel-whl python3-setuptools-whl python3-pip-whl gstreamer1.0-plugins-bad &&
 	
 	printf "\n" &&
 	printf "${GRE}Done!\n" &&
@@ -279,14 +279,16 @@ esac
 
 printf "\n" &&
 printf "${bold}${GRE}Script to install useful packages on Ubuntu.${c0}\n" &&
-printf "${bold}${YEL}Use the --lib32 flag to enable i386 MultiArch support (Recommended to run this before the others).${c0}\n" &&
-printf "${bold}${YEL}Use these flags for the Ubuntu release running on your system.${c0}\n" &&
 printf "\n" &&
-printf "${bold}${YEL}--xenial (Ubuntu 16.04, Codename \"Xenial Xerus\".${c0}\n" &&
-printf "${bold}${YEL}--bionic (Ubuntu 18.04, Codename \"Bionic Beaver\".${c0}\n" &&
-printf "${bold}${YEL}--focal (Ubuntu 20.04, Codename \"Focal Fossa\".${c0}\n" &&
-printf "${bold}${YEL}--jammy (Ubuntu 22.04, Codename \"Jammy Jellyfish\".${c0}\n" &&
-printf "${bold}${YEL}--lunar (Ubuntu 23.04, Codename \"Lunar Lobster\".${c0}\n" &&
+printf "${bold}${YEL}Use the --lib32 flag to enable 32 bit support. (Recommended to run this first)${c0}\n" &&
+printf "${bold}${YEL}Use these flags for your corresponding Ubuntu release:${c0}\n" &&
+printf "\n" &&
+printf "${bold}${YEL}  --xenial | Ubuntu 16.04, Codename \"Xenial Xerus\"${c0}\n" &&
+printf "${bold}${YEL}  --bionic | Ubuntu 18.04, Codename \"Bionic Beaver\"${c0}\n" &&
+printf "${bold}${YEL}  --focal  | Ubuntu 20.04, Codename \"Focal Fossa\"${c0}\n" &&
+printf "${bold}${YEL}  --jammy  | Ubuntu 22.04, Codename \"Jammy Jellyfish\"${c0}\n" &&
+printf "${bold}${YEL}  --lunar (Ubuntu 23.04, Codename \"Lunar Lobster\".${c0}\n" &&
+printf "${bold}${YEL}  --debian (Debian 12.1, Codename \"Bookworm\".${c0}\n" &&
 printf "\n"
 
 exit 0
